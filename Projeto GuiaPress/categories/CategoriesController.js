3cb04b829512c410a50c1c942a450b2ddb5024e7
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-13 15:33:56 -0300

    Cadastro de categorias

--- Projeto GuiaPress/categories/CategoriesController.js
+++ Projeto GuiaPress/categories/CategoriesController.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const slugify = require('slugify');
+const Category = require('./Category');
 
 router.get('/admin/categories/new', (req, res) => {
     res.render('admin/categories/new');
+});
+
+router.post('/categories/save', (req, res) => {
+    const title = req.body.title;
+
+    if(title != undefined) {
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect('/');
+        })
+    } else {
+        res.redirect('/admin/categories/new');
+    }
 });
 
 module.exports = router;
