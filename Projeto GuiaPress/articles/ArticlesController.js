da0385022ddb4a10c22fef1b10a52cd2565f276a
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-14 17:55:25 -0300

    Página de artigos

--- Projeto GuiaPress/articles/ArticlesController.js
+++ Projeto GuiaPress/articles/ArticlesController.js
@@ -5,7 +5,11 @@ const Article = require('./Article');
 const slugify = require('slugify');
 
 router.get('/admin/articles', (req, res) => {
-    res.send('Rota de artigos');
+    Article.findAll({
+        include: [{ model: Category }]
+    }).then(articles => {
+        res.render('admin/articles/index', { articles: articles })
+    });
 });
 
 router.get('/admin/articles/new', (req, res) => {
@@ -27,6 +31,26 @@ router.post('/articles/save', (req, res) => {
     }).then(() => {
         res.redirect('/admin/articles')
     });
+});
+
+router.post('/articles/delete', (req, res) => {
+    const id = req.body.id;
+    
+    if(id != undefined) {
+        if(!isNaN(id)) {
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect('/admin/articles');
+            });
+        } else {
+            res.redirect('/admin/articles');
+        }
+    } else {
+        res.redirect('/admin/articles');
+    }
 });
 
 module.exports = router;
