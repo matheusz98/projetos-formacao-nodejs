507e30716feff67c9b733fd13e50ad7021a76c42
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-14 17:23:55 -0300

    Salvando artigos

--- Projeto GuiaPress/articles/ArticlesController.js
+++ Projeto GuiaPress/articles/ArticlesController.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../categories/Category');
+const Article = require('./Article');
+const slugify = require('slugify');
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
     res.send('Rota de artigos');
 });
 
 router.get('/admin/articles/new', (req, res) => {
     Category.findAll().then(categories => {
         res.render('admin/articles/new', {categories: categories})
+    });
+});
+
+router.post('/articles/save', (req, res) => {
+    const title = req.body.title;
+    const body = req.body.body;
+    const category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect('/admin/articles')
     });
 });
 
