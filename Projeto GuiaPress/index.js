9f41be4056ea792df4c9ce162aee8de43571279a
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-14 23:24:06 -0300

    Menu dinãmico de categorias

--- Projeto GuiaPress/index.js
+++ Projeto GuiaPress/index.js
@@ -32,7 +32,9 @@ app.get('/', (req, res) => {
             ['id', 'DESC']
         ]
     }).then(articles => {
-        res.render('index', { articles: articles })
+        Category.findAll().then(categories => {
+            res.render('index', { articles: articles, categories: categories });
+        });
     });
 });
 
@@ -45,7 +47,9 @@ app.get('/:slug', (req, res) => {
         }
     }).then(article => {
         if(article != undefined) {
-            res.render('article', { article: article });
+            Category.findAll().then(categories => {
+                res.render('article', { article: article, categories: categories });
+            });
         } else {
             res.redirect('/');
         }
