5a88f0a2ae99ec5d4ee3ad7894bf9398c43b034d
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-14 23:42:56 -0300

    Filtrando artigos por categoria

--- Projeto GuiaPress/index.js
+++ Projeto GuiaPress/index.js
@@ -58,6 +58,27 @@ app.get('/:slug', (req, res) => {
     });
 });
 
+app.get('/category/:slug', (req, res) => {
+    const slug = req.params.slug;
+
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{ model: Article }]
+    }).then(category => {
+        if(category != undefined) {
+            Category.findAll().then(categories => {
+                res.render('index', { articles: category.articles, categories: categories });
+            });
+        } else {
+            res.redirect('/');
+        }
+    }).catch(error => {
+        res.redirect('/');
+    });
+});
+
 app.listen(8080, () => {
     console.log('Servidor iniciado com sucesso!');
 });
