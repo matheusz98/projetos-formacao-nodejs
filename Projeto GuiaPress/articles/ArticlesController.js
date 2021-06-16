9783bcba3d03cb959591a7bc8febd5d66bfc8b47
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-16 17:52:15 -0300

    Middleware de autenticação

--- Projeto GuiaPress/articles/ArticlesController.js
+++ Projeto GuiaPress/articles/ArticlesController.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const Category = require('../categories/Category');
 const Article = require('./Article');
 const slugify = require('slugify');
+const adminAuth = require('../middlewares/adminAuth');
 
-router.get('/admin/articles', (req, res) => {
+router.get('/admin/articles', adminAuth, (req, res) => {
     Article.findAll({
         include: [{ model: Category }]
     }).then(articles => {
@@ -12,7 +13,7 @@ router.get('/admin/articles', (req, res) => {
     });
 });
 
-router.get('/admin/articles/new', (req, res) => {
+router.get('/admin/articles/new', adminAuth, (req, res) => {
     Category.findAll().then(categories => {
         res.render('admin/articles/new', {categories: categories})
     });
@@ -33,7 +34,7 @@ router.post('/articles/save', (req, res) => {
     });
 });
 
-router.post('/articles/delete', (req, res) => {
+router.post('/articles/delete', adminAuth, (req, res) => {
     const id = req.body.id;
     
     if(id != undefined) {
@@ -53,7 +54,7 @@ router.post('/articles/delete', (req, res) => {
     }
 });
 
-router.get('/admin/articles/edit/:id', (req, res) => {
+router.get('/admin/articles/edit/:id', adminAuth, (req, res) => {
     const id = req.params.id;
 
     Article.findByPk(id).then(article => {
@@ -69,7 +70,7 @@ router.get('/admin/articles/edit/:id', (req, res) => {
     });
 });
 
-router.post('/articles/update', (req, res) => {
+router.post('/articles/update', adminAuth, (req, res) => {
     const id = req.body.id;
     const title = req.body.title;
     const body = req.body.body;
