9ced5e087b4d689e10d8a005fabe4c0c3d9ff728
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-17 23:23:42 -0300

    Leitor/Escritor de arquivo JSON

--- Projeto Guia Converter/index.js
+++ Projeto Guia Converter/index.js
@@ -1,7 +1,34 @@
 const fs = require('fs');
 
-fs.writeFile('./matheus.costa', 'skidaddle skadoodle', (error) => {
+/* fs.readFile('./usuario.json', { encoding: 'utf-8'}, (error, data) => {
     if(error) {
-        console.log('Erro durante a escrita');
+        console.log('Deu ruim');
+    } else {
+        console.log(data);
+    }
+}); */
+
+/* fs.readFile('./usuario.json', { encoding: 'utf-8'}, (error, data) => {
+    if(error) {
+        console.log('Deu ruim');
+    } else {
+        const text = JSON.parse(data);
+        console.log(text);
+    }
+}); */
+
+fs.readFile('./usuario.json', { encoding: 'utf-8'}, (error, data) => {
+    if(error) {
+        console.log('Deu ruim');
+    } else {
+        const text = JSON.parse(data);
+
+        text.stack = 'Desenvolvimento Front-End';
+
+        fs.writeFile('./usuario.json', JSON.stringify(text), (error) => {
+            if(error) {
+                console.log('Deu ruim de novo');
+            }
+        })
     }
 });
