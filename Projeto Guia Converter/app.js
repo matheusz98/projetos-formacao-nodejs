c418b20f39f972c3433bc1898fd2287c70439639
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-18 02:53:26 -0300

    PDFWriter e finalização do projeto

--- Projeto Guia Converter/app.js
+++ Projeto Guia Converter/app.js
@@ -3,6 +3,7 @@ const Writer = require('./Writer');
 const Processor = require('./Processor');
 const Table = require('./Table');
 const HtmlParser = require('./HtmlParser');
+const PDFWriter = require('./PDFWriter');
 
 const fileReader = new Reader();
 const writer = new Writer();
@@ -15,6 +16,7 @@ async function main() {
     const html = await HtmlParser.Parse(users);
 
     writer.Write(Date.now() + '.html', html);
+    PDFWriter.WritePDF(Date.now() + '.PDF', html);
 }
 
 main();
