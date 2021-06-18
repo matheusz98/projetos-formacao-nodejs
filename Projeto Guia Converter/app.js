28c31270e9a815c42379378fafec56fb41068fce
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-18 01:13:44 -0300

    Table

--- Projeto Guia Converter/app.js
+++ Projeto Guia Converter/app.js
@@ -1,11 +1,15 @@
 const Reader = require('./Reader');
 const Processor = require('./Processor');
+const Table = require('./Table');
 
 const fileReader = new Reader();
 
 async function main() {
     const data = await fileReader.Read('./planilha.csv');
     const processData = Processor.Process(data);
+    const users = new Table(processData);
+
+    console.log(users.rows);
 }
 
 main();
