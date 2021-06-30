7c173ba297be8ff0dc08fb2e920ca98c75bfcf68
matheusz98 <matheuscastiglia31@gmail.com>
2021-06-30 00:36:50 -0300

    Cadastrando dados

--- Projeto API REST/index.js
+++ Projeto API REST/index.js
@@ -51,6 +51,19 @@ app.get('/game/:id', (req, res) => {
     }
 });
 
+app.post('/game', (req, res) => {
+    const { title, price, year } = req.body;
+
+    DB.games.push({
+        id: 4,
+        title,
+        price,
+        year
+    });
+
+    res.sendStatus(200);
+});
+
 app.listen(3000, () => {
     console.log('Servidor inicializado com sucesso!');
 })
