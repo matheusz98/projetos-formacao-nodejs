3547af27696fbd16bd1cb3a771de390d7641cd5b
matheusz98 <matheuscastiglia31@gmail.com>
2021-07-01 01:57:40 -0300

    Rota de autenticação

---
+++ Projeto API REST com autenticação JWT/index.js
@@ -0,0 +1,156 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const DB = {
+    games: [
+        { 
+            id: 1,
+            title: 'Metal Gear Solid 4: Guns of the Patriots',
+            year: 2008,
+            price: 30,
+        },
+
+        { 
+            id: 2,
+            title: 'Red Dead Redemption II',
+            year: 2018,
+            price: 50,
+        },
+
+        { 
+            id: 3,
+            title: 'Persona 5 Royal',
+            year: 2020,
+            price: 40,
+        },
+    ],
+
+    users: [
+        {
+            id: 1,
+            name: 'Matheus Costa',
+            email: 'matheus@gmail.com',
+            password: '784512js'
+        },
+        {
+            id: 2,
+            name: 'Guilherme Fidelis',
+            email: 'fidelisamigao@gmail.com',
+            password: '9841561db',
+        }
+    ]
+}
+
+app.get('/games', (req, res) => {
+    res.statusCode = 200;
+    res.json(DB.games);
+});
+
+app.get('/game/:id', (req, res) => {
+    if(isNaN(req.params.id)) {
+        res.sendStatus(400);
+    } else {
+        const id = parseInt(req.params.id);
+
+        const game = DB.games.find(game => game.id == id);
+
+        if(game != undefined) {
+            res.statusCode = 200;
+            res.json(game);
+        } else {
+            res.sendStatus(404);
+        }
+    }
+});
+
+app.post('/game', (req, res) => {
+    const { title, price, year } = req.body;
+
+    DB.games.push({
+        id: 4,
+        title,
+        price,
+        year
+    });
+
+    res.sendStatus(200);
+});
+
+app.delete('/game/:id', (req, res) => {
+    if(isNaN(req.params.id)) {
+        res.sendStatus(400);
+    } else {
+        const id = parseInt(req.params.id);
+        var index = DB.games.findIndex(game => game.id == id);
+
+        if(index == -1) {
+            res.sendStatus(404);
+       } else {
+           DB.games.splice(index, 1);
+           res.sendStatus(200);
+       }
+    }
+});
+
+app.put('/game/:id', (req, res) => {
+    if(isNaN(req.params.id)) {
+        res.sendStatus(400);
+    } else {
+        const id = parseInt(req.params.id);
+
+        const game = DB.games.find(game => game.id == id);
+
+        if(game != undefined) {
+            const { title, price, year } = req.body;
+
+            if(title != undefined) {
+                game.title = title;
+            }
+
+            if(price != undefined) {
+                game.price = price;
+            }
+
+            if(year != undefined) {
+                game.year = year;
+            }
+
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(404);
+        }
+    }
+});
+
+app.post('/auth', (req, res) => {
+    const { email, password } = req.body;
+
+    if(email != undefined) {
+        const user = DB.users.find(u => u.email == email);
+
+        if(user != undefined) {
+            if(user.password == password) {
+                res.status = 200;
+                res.json({ token: 'Token falso!'});
+            } else {
+                res.status = 401;
+                res.json({ error: 'Credenciais inválidas!'});
+            }
+        } else {
+            res.status = 404;
+            res.json({ error: 'O e-mail informado não existe em nosso banco de dados'});
+        }
+    } else {
+        res.status = 400;
+        res.json({ error: 'O e-mail informado é inválido'});
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Servidor inicializado com sucesso!');
+})
