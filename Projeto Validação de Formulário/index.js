db72e56bcffbe758d10141e018ce1a62c84912c4
matheusz98 <matheuscastiglia31@gmail.com>
2021-07-03 00:16:06 -0300

    Validando formulário

--- Projeto Validação de Formulário/index.js
+++ Projeto Validação de Formulário/index.js
@@ -18,8 +18,37 @@ app.use(session({
 app.use(flash());
 
 app.get('/', (req, res) => {
-    console.log('Funfando!');
-    res.send('Funfando!');
+    res.render('index');
+});
+
+app.post('/form', (req, res) => {
+    let { email, name, pontos } = req.body;
+
+    let emailError;
+    let nameError;
+    let pontosError;
+
+    if(email == undefined || email == '') {
+        emailError = 'O campo e-mail não pode ser vazio';
+    }
+
+    if(name == undefined || name == '') {
+        nameError = 'O nome não pode ser vazio';
+    }
+
+    if(name.length < 4) {
+        nameError = 'O nome é muito pequeno';
+    }
+
+    if(pontos == undefined || pontos < 20) {
+        pontosError = 'Você não pode ter menos de 20 pontos';
+    }
+
+    if(emailError != undefined || pontosError != undefined || nameError != undefined) {
+        res.redirect('/');
+    } else {
+        res.send('Funfou!');
+    }
 });
 
 app.listen(3000,(req, res) => {
