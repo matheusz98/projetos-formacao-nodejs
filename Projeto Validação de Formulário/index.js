88f17691d3b75367ce881dea6ab9b1aa4d3ca168
matheusz98 <matheuscastiglia31@gmail.com>
2021-07-03 00:30:38 -0300

    Cookie Parser

--- Projeto Validação de Formulário/index.js
+++ Projeto Validação de Formulário/index.js
@@ -2,17 +2,20 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const flash = require('express-flash');
+const cookieParser = require('cookie-parser');
 
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(cookieParser('asoijaehiuah'));
+
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
+    cookie: { maxAge: 60000 }
 }));
 
 app.use(flash());
