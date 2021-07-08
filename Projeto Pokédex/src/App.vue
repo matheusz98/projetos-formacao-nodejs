fff93f9cd55c8529030cbe0a63f4fd3653359411
matheusz98 <matheuscastiglia31@gmail.com>
2021-07-08 11:50:35 -0300

    Componente Pokemon

--- Projeto Pokédex/src/App.vue
+++ Projeto Pokédex/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
-
+    <div v-for='(poke, index) in pokemons' :key='index'>
+      <Pokemon :name='poke.name' :url='poke.url' :num='index+1'/>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Pokemon from './components/Pokemon';
 export default {
   name: 'App',
   data() {
@@ -18,6 +21,10 @@ export default {
     axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res => {
       this.pokemons = res.data.results;
     })
+  },
+
+  components: {
+    Pokemon
   }
 }
 </script>
---
+++ Projeto Pokédex/src/components/Pokemon.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{ num }} {{ upper(name) }}</h1>
+        <small>{{ url }}</small>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        num: Number,
+        name: String,
+        url: String
+    },
+
+    methods: {
+        upper: function(value) {
+            const upperCase = value[0].toUpperCase() + value.slice(1);
+            return upperCase;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
